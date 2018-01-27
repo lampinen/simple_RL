4990558a7a1cc4f2fa13f579c66a745063d2aa7a
Andrew Lampinen <lampinen@stanford.edu>
2018-01-26 17:52:07 -0800

    Correct output format

--- cartpole.py
+++ cartpole.py
@@ -111,7 +111,7 @@ class cartpole_problem(object):
                                        frames=trial_state_history,
                                        interval=1000./ticks_per_second,
                                        repeat=False)
-        display(HTML(anim.to_html5_video()))
+        display(HTML(anim.to_jshtml()))
 
     def run_trial(self, controller, testing=False, animate=False):
         self.reset_state()
